9aa86d2ac5eab397825fc0fe34d01e3543e36a71
zachbanderson <zachbanderson@gmail.com>
2025-07-02 15:04:35 -0700

    Make hitboxes disable upon taking damage

--- scripts/enemy_template.gd
+++ scripts/enemy_template.gd
@@ -15,14 +15,13 @@ func _ready() -> void:
 # Handles the enemy taking damage.
 func take_damage(damage: float):
 	curr_health -= damage
+	for hitbox in hitboxes_to_disable_on_death:
+		hitbox.set_deferred("disabled", true)
 	check_for_death()
 
 # Checks if the enemy's health is less than or equal to 0. Then should die.
 func check_for_death():
 	if curr_health <= 0:
-		death_flag = true
-		for hitbox in hitboxes_to_disable_on_death:
-			hitbox.set_deferred("disabled", true)
 		$AnimationPlayer.queue("Shared_Enemy_Animations/die")
 	else:
 		$AnimationPlayer.queue("Shared_Enemy_Animations/take_damage")
@@ -33,6 +32,10 @@ func die():
 	queue_free()
 	
 func _on_animation_player_animation_finished(anim_name: String):
-	if anim_name == 'Shared_Enemy_Animations/die':
-		if death_flag == true:
-			die()
+	# If the death animation plays, then the enemy should die upon completion.
+	if anim_name == "Shared_Enemy_Animations/die":
+		die()
+	# If the enemy only takes damage, re-enable the hitboxes.
+	elif anim_name == "Shared_Enemy_Animations/take_damage":
+		for hitbox in hitboxes_to_disable_on_death:
+			hitbox.set_deferred("disabled", false)
